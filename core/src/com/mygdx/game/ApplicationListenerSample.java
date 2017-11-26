c2c20a2082dccaa90a912cd4b716028e323b53a5
mario buonomo <ma.buonomo@gmail.com>
2017-11-26 15:01:16 +0100

    test

--- core/src/com/mygdx/game/ApplicationListenerSample.java
+++ core/src/com/mygdx/game/ApplicationListenerSample.java
@@ -1,41 +1,56 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.Logger;
+
+import java.util.logging.Level;
 
 public class ApplicationListenerSample implements ApplicationListener {
 
-	@Override
-	public void create() {
+    private static final String TAG = "ApplicationListernerSample";
+    private static final Logger logger = new Logger(ApplicationListener.class.getName(), Logger.DEBUG);
+    private boolean renderInterrupt = true;
 
-	}
+    @Override
+    public void create() {
+        Gdx.app.setLogLevel(Application.LOG_DEBUG);
+        logger.debug("create()");
+    }
 
-	@Override
-	public void resize(int width, int height) {
+    @Override
+    public void resize(int width, int height) {
+        logger.debug("resize () width= " + width + " height=" + height);
+    }
 
-	}
+    @Override
+    public void render() {
+        // called 60 times for seconds
+        if (renderInterrupt) {
+            logger.debug("render()");
+            renderInterrupt = false;
+        }
+    }
 
-	@Override
-	public void render() {
+    @Override
+    public void pause() {
+        logger.debug("pause()");
+        renderInterrupt = true;
+    }
 
-	}
+    @Override
+    public void resume() {
+        logger.debug("resume()");
+        renderInterrupt = true;
+    }
 
-	@Override
-	public void pause() {
-
-	}
-
-	@Override
-	public void resume() {
-
-	}
-
-	@Override
-	public void dispose() {
-
-	}
+    @Override
+    public void dispose() {
+        logger.debug("dispose()");
+    }
 }
